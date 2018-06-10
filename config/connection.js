768d8267f395300fed1f2224e29f1f3bf37ed5b3
tlo015 <tlo015@yahoo.com>
2018-06-09 23:18:12 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  })
+}
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
